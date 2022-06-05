d63c44fd39ca39b7165c55bd434e81a9743dba75
prasetyomuhdwi <muhammad.2365@students.amikom.ac.id>
2022-06-05 18:18:49 +0700

    use nodejs framework @hapi/hapi

--- server.js
+++ server.js
@@ -1,80 +1,13 @@
-const http = require("http");
+const Hapi = require("@hapi/hapi");
 
-/**
- * @param request: objek yang berisikan informasi terkait permintaan
- * @param response: objek yang digunakan untuk menanggapi permintaan
- */
-const requestListener = (request, response) => {
-  response.setHeader("Content-Type", "text/html");
-  response.setHeader("X-Powered-By", "NodeJS");
-  response.statusCode = 200;
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
 
-  const { method, url } = request;
-
-  if (url === "/") {
-    if (method === "GET") {
-      response.statusCode = 200;
-      response.end(
-        JSON.stringify({
-          message: "Ini adalah homepage",
-        })
-      );
-    } else {
-      response.statusCode = 400;
-      response.end(
-        JSON.stringify({
-          message: `Halaman tidak dapat diakses dengan ${method} request`,
-        })
-      );
-    }
-  } else if (url === "/about") {
-    if (method === "GET") {
-      response.statusCode = 200;
-      response.end(
-        JSON.stringify({
-          message: "Halo! Ini adalah halaman about",
-        })
-      );
-    } else if (method === "POST") {
-      let body = [];
-
-      request.on("data", (chunk) => {
-        body.push(chunk);
-      });
-
-      request.on("end", () => {
-        body = Buffer.concat(body).toString();
-        const { name } = JSON.parse(body);
-        response.statusCode = 200;
-        response.end(
-          JSON.stringify({
-            message: `Halo, ${name}! Ini adalah halaman about`,
-          })
-        );
-      });
-    } else {
-      response.statusCode = 400;
-      response.end(
-        JSON.stringify({
-          message: `Halaman tidak dapat diakses menggunakan ${method} request`,
-        })
-      );
-    }
-  } else {
-    response.statusCode = 404;
-    response.end(
-      JSON.stringify({
-        message: "Halaman tidak ditemukan!",
-      })
-    );
-  }
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
-const server = http.createServer(requestListener);
-
-const port = 5000;
-const host = "localhost";
-
-server.listen(port, host, () => {
-  console.log(`Server berjalan pada http://${host}:${port}`);
-});
+init();
